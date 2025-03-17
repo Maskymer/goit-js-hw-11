b2deba647494a987e7b6a7bb4c613872376a9a5d
Maksym <makseemer@gmail.com>
2025-03-17 19:26:39 +0200

    fixed

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 import SimpleLightbox from 'simplelightbox'; 
 import 'simplelightbox/dist/simple-lightbox.min.css'; 
-import { fetchImages } from './js/pixaby-api';
+import { fetchImages } from './js/pixabay-api';
 import {  
   renderImages,  
   showLoader,  
